306a90f6eba4b8efd1fdf37285183bb261632482
Andrew Sunada <kaleb2azriel@gmail.com>
2020-10-16 11:30:56 -0700

    setting up project for initial changes

---
+++ src/components/app/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Houses to Homes</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
