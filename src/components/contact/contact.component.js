875c757ad9983027576a7fe8ddf60e1fa8e5a7ec
Andrew Sunada <kaleb2azriel@gmail.com>
2020-10-22 23:21:16 -0700

    adding contact info page

--- src/components/contact/contact.component.js
+++ src/components/contact/contact.component.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import Header from '../header/header.component';
+import ProfileCard from '../profile-card/profile-card.component';
 import styles from './contact.module.scss';
 
 export default function Contact() {
+  const profiles = [
+    {
+      image: '/images/andrea.jpg',
+      email: 'andrea@housestohomesresidential.com',
+      name: 'Andrea Sunada',
+      role: 'Founding Partner',
+      phone: '(503) 410-1829',
+    },
+    {
+      image: '/images/andrew.jpg',
+      email: 'andrew@housestohomesresidential.com',
+      name: 'Andrew Sunada',
+      role: 'Founding Partner',
+      phone: '(503) 475-9019',
+    },
+  ];
   return (
     <div className={styles.container}>
       <Header />
+      <h2 className={styles.header}>Contact Info</h2>
+      <section className={styles.content}>
+        {profiles.map((profile, i) => (
+          <ProfileCard profile={profile} key={i} />
+        ))}
+      </section>
     </div>
   );
 }
--- src/components/profile-card/profile-card.component.js
+++ src/components/profile-card/profile-card.component.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import styles from './profile-card.module.scss';
 
-export default function ProfileCard() {
-  return <div className={styles.card}>Hello world</div>;
+export default function ProfileCard({ profile }) {
+  const { image, email, name, role, phone } = profile;
+  return (
+    <div className={styles.card}>
+      <img className={styles.image} src={image} alt="Profile" />
+      <section className={styles.info}>
+        <span className={styles.arrow}></span>
+        <p className={styles.title}>{name}</p>
+        <p className={styles.subtitle}>{role}</p>
+        <p className={styles['info-primary']}>{phone}</p>
+        <p className={styles['info-secondary']}>
+          https://housestohomesresidential.com
+        </p>
+        <p className={styles['info-tertiary']}>{email}</p>
+      </section>
+    </div>
+  );
 }
